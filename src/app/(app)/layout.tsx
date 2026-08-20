e8ed79ba14c5a452745eece6e327dc2b1fda38e4
Jepjep18 <jeparnado@gmail.com>
2026-08-21 00:40:16 +0800

    fix(layout): correct class for full-height layout in AppLayout component

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -9,7 +9,7 @@ interface AppLayoutProps {
 
 export default function AppLayout({ children }: AppLayoutProps) {
   return (
-    <div className="flex min-h-screen bg-[#051424]">
+    <div className="flex h-screen bg-[#051424]">
       <AppSidebar />
       <div className="flex flex-1 flex-col min-h-0 overflow-hidden">
         {children}
